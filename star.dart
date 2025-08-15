65354e788951a0477084174d34edd0631accba61
dcaley <davidcaley@gmail.com>
2025-08-14 22:12:48 -0700

    whoops

--- star.dart
+++
@@ -1,8 +0,0 @@
-import 'body.dart';
-
-class Star extends Body{
-  Star({required super.position, required super.velocity, super.mass=1, super.color, super.offset});
-
-  @override
-  double get paintSize => 2;
-}
