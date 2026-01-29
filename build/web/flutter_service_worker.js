8a3d132170d6244a10898017e09c5e48c3e5dc75
dcaley <davidcaley@gmail.com>
2026-01-28 22:49:27 -0800

    Actually make use of vector_math in our setup.

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "4b497b9c1f3a880ae56ae9e7fc174c02",
+"flutter_bootstrap.js": "49b94bbd50ae68bb157348e733e335b0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
@@ -14,7 +14,7 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "manifest.json": "b52e6100a18626e993dcfe3f3d85f551",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "ffb6f99b9c704b1c9f25a7407c2e0545",
-"main.dart.js": "84f71231b174c78de408364821bad0bb",
+"main.dart.js": "6a940e235d7ebb73ea5c62c4e1e0632d",
 "canvaskit/skwasm_heavy.js": "740d43a6b8240ef9e23eed8c48840da4",
 "canvaskit/canvaskit.js": "8331fe38e66b3a898c4f37648aaf7ee2",
 "canvaskit/skwasm_heavy.js.symbols": "0755b4fb399918388d71b59ad390b055",
@@ -34,7 +34,6 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/NOTICES": "6bfd14c0110f89f35df407ff909db43b"};
 // The application shell files that are downloaded before a service worker can
 // start.
